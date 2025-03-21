a9dffefc4793fd80d8c8fa4d8413e54c7d508ccb
Menaamoon <jimenacelayamora@gmail.com>
2025-03-21 15:41:08 -0600

    Update config.php

--- sistemaDeHotel/config.php
+++ sistemaDeHotel/config.php
@@ -1,12 +1,1 @@
-<?php
-$host = 'localhost';
-$datanombre = 'hotelReservas';
-$usuario = 'root';
-$contraseña = '';
 
-$conn = new mysqli($host, $usuario, $contraseña, $datanombre);
-
-if ($conn->connect_error) {
-    die('Error de conexión: ' . $conn->connect_error);
-}
-?>
